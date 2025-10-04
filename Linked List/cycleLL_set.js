888a8f15dbac67134e9e979fc14cdf157c834b24
NaushinK <naushink2709@gmail.com>
2025-10-05 01:24:59 +0530

    linkedList problems

---
+++ Linked List/cycleLL_set.js
@@ -0,0 +1,12 @@
+function cycleLL_set(head){
+    let set=new Set();
+    let curr=head;
+    while(curr){
+        if(set.has(curr)){
+            return true;
+        }
+        set.add(curr);
+        curr=curr.next;
+    }
+    return false;
+}
