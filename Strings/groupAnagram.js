70f7f7618cf88f93ab3e8a4d8713641b4e390ea0
NaushinK <naushink2709@gmail.com>
2025-11-12 00:39:29 +0530

    group Anagram

---
+++ Strings/groupAnagram.js
@@ -0,0 +1,22 @@
+var groupAnagram=function (strs){
+    let map={};
+
+    //Iterating over each string.
+
+    for(let i=0;i<strs.length;i++){
+
+        let sortedStr= strs[i].split("").sort().join("");
+
+        if(!map[sortedStr]){
+            map[sortedStr]=[strs[i]];
+        }
+        else{
+            map[sortedStr].push(strs[i])
+        }
+
+    }
+
+    return [... Object.values(map)]
+}
+
+console.log(groupAnagram(["eat","tea","tan","ate","nat","bat"]))
