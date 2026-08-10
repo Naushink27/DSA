2a4f2ba10989d84b0f47030bb6eca957d0e582de
NaushinK <naushink2709@gmail.com>
2026-08-11 01:32:01 +0530

    Trees started

---
+++ 02.postOrder.js
@@ -0,0 +1,74 @@
+/**
+ * Binary Tree Node
+ */
+function TreeNode(val, left = null, right = null) {
+    this.val = val;
+    this.left = left;
+    this.right = right;
+}
+
+
+/**
+ * Create a complex binary tree
+ *
+ *                 1
+ *              /     \
+ *             2       3
+ *           /  \     / \
+ *          4    5   6   7
+ *         / \    \      / \
+ *        8   9   10    11 12
+ *           /          \
+ *          13           14
+ *                        \
+ *                         15
+ */
+const root = new TreeNode(
+    1,
+    new TreeNode(
+        2,
+        new TreeNode(
+            4,
+            new TreeNode(8),
+            new TreeNode(
+                9,
+                new TreeNode(13),
+                null
+            )
+        ),
+        new TreeNode(
+            5,
+            null,
+            new TreeNode(10)
+        )
+    ),
+    new TreeNode(
+        3,
+        new TreeNode(6),
+        new TreeNode(
+            7,
+            new TreeNode(
+                11,
+                null,
+                new TreeNode(14, null, new TreeNode(15))
+            ),
+            new TreeNode(12)
+        )
+    )
+);
+
+console.log(root);
+
+var postOrder=function (root) {
+    let ans=[]
+    function  traversal(curr) {
+        if (!curr) return;
+        
+        traversal(curr.left)
+        traversal(curr.right)
+        ans.push(curr.val)
+    }
+    traversal(root)
+    return ans;
+}
+console.log(postOrder(root))
